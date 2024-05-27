48e28f50255fb1c670d95cbe56ce44d85a70079f
miharayukito <mihara2994@gmail.com>
2024-05-27 10:09:21 +0900

    stlike2

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -17,18 +17,27 @@ class Bowling
     def add_score(pins)
         # 一時保存用のスコアに倒したピンの数を追加する
         @temp << pins
-        # ２段分のデータが入っていれば、１フレーム分のスコアとして全体に追加する
-        if @temp.size == 2
-            @scores << @temp
-            @temp = []
+        # ２段分のデータが入っているか、１段目がストライクだったら、１フレーム分のスコアとして全体に追加する
+        if @temp.size == 2 || strike?(@temp)
+          @scores << @temp
+          @temp = []
         end
     end
 
     # スコアの合計を計算する
   def calc_score
     @scores.each.with_index(1) do |score, index|
+      # 最終フレーム以外でのストライクなら、スコアにボーナスを含めて合計する
+      if strike?(score) && not_last_frame?(index)
+        # 次のフレームもストライクで、なおかつ最終フレーム以外なら、
+        # もう一つ次のフレームの一投目をボーナスの対象にする
+        if strike?(@scores[index]) && not_last_frame?(index + 1)
+          @total_score += 20 + @scores[index + 1].first
+        else
+          @total_score += 10 + @scores[index].inject(:+)
+        end
       # 最終フレーム以外でのスペアなら、スコアにボーナスを含めて合計する
-      if spare?(score) && not_last_frame?(index)
+      elsif spare?(score) && not_last_frame?(index)
         @total_score += calc_spare_bonus(index)
       else
         @total_score += score.inject(:+)
@@ -42,6 +51,11 @@ class Bowling
     score.inject(:+) == 10
   end
 
+  # ストライクかどうか判定する
+  def strike?(score)
+    score.first == 10
+  end
+  
   # 最終フレーム以外かどうか判定する
   def not_last_frame?(index)
     index < 10
